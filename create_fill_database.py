89122d1b6a8bae4eccb5f472b56dccb6192cb659
AdamSanch <adamsan1234@gmail.com>
2023-12-07 20:17:29 -0500

    create_table function working and add merged.csv file

--- create_fill_database.py
+++ create_fill_database.py
@@ -24,50 +24,13 @@ imdb_rating_file = 'IMDB-data-BIG/title.ratings.tsv'
 database_name = 'movie_recommender.db'
 
 
-# Merge movie table with IMDB data
-def add_IMDB_data(file_name):
-    with sqlite3.connect(database_name) as conn:
-        cursor = conn.cursor()
-        if file_name == imdb_rating_file:
-            with open(file_name, 'r', encoding='utf-8') as tsvfile:
-                reader = csv.reader(tsvfile, delimiter='\t')
-                next(reader)
-                for row in tqdm(reader):
-                    # Check if the 'Imdb_id' variable, with the first 2 letters removed and the rest converted to int, exists in the 'movies' table 
-                    Imdb_id = int(row[0][2:])
-                    cursor.execute("SELECT 1 FROM movies WHERE IMDB_id=?", (Imdb_id,))
-                    data = cursor.fetchone()
-                    if data is not None:
-                        # If the 'Imdb_id' variable exists in the 'movies' table, update the 'avgRating_IMDB' and 'numVotes_IMDB' variables
-                        avgRating = None if row[1] == '\\N' else float(row[1])
-                        nVotes = None if row[2] == '\\N' else int(row[2]) 
-                        if avgRating is not None and nVotes is not None:
-                            cursor.execute("UPDATE movies SET avgRating_IMDB=?, numVotes_IMDB=? WHERE IMDB_id=?", (avgRating, nVotes, Imdb_id))
-                            print(f'Updated numVotes and avgRating in ratingFile to {nVotes} and {avgRating} respectively with IMDB id {Imdb_id}')
-                        else:
-                            print('***!!!Could not update movie in ratingFile with IMDB id', Imdb_id)
-        
-        elif file_name == imdb_movie_file:
-            with open(file_name, 'r', encoding='utf-8') as tsvfile:
-                reader = csv.reader(tsvfile, delimiter='\t')
-                next(reader)
-                for row in tqdm(reader):
-                    # Check if the 'Imdb_id' variable, with the first 2 letters removed and the rest converted to int, exists in the 'movies' table 
-                    Imdb_id = int(row[0][2:])
-                    cursor.execute("SELECT 1 FROM movies WHERE IMDB_id=?", (Imdb_id,))
-                    data = cursor.fetchone()
-                    if data is not None:
-                        # If the movie exists in the 'movies' table, update the 'is_adult' and 'runtime_min' variables
-                        is_adult = None if row[4] == '\\N' else int(row[4])
-                        runtime_min = None if row[7] == '\\N' else int(row[7])
-                        if is_adult is not None and runtime_min is not None:
-                            cursor.execute("UPDATE movies SET is_adult=?, runtime_min=? WHERE IMDB_id=?", (is_adult, runtime_min, Imdb_id))
-                            print(f'Updated is_adult and runtime_min in movieFile to {is_adult} and {runtime_min} respectively with IMDB id {Imdb_id}')
-                        else:
-                            print('***!!!Could not update movie in movieFile with IMDB id ', Imdb_id)
+"""
+------------------------------------------------------------------------------------------------------------------------
+    Functions to create and populate the database with csv files
+------------------------------------------------------------------------------------------------------------------------
+"""
 
-
-# Create and populate movie table using movies.csv, and links.csv, then add IMDB data 
+# Create and populate movie table using movies.csv, and links.csv 
 def create_movie_table():
     with sqlite3.connect(database_name) as conn:
         cursor = conn.cursor()
@@ -91,8 +54,9 @@ def create_movie_table():
                 Imdb_id = int(row[1])
                 cursor.execute("UPDATE movies SET IMDB_id=? WHERE movie_id=?", (Imdb_id, movie_id))
         
-    add_IMDB_data(imdb_rating_file)
-    add_IMDB_data(imdb_movie_file)
+        # Create index on IMDB_id variable in movies table
+        cursor.execute("CREATE INDEX IF NOT EXISTS idx_movies_imdb_id ON movies (IMDB_id)")
+        print('Created index on IMDB_id in movies table\n')
 
 
 # Create and populate ratings table using ratings.csv
@@ -120,25 +84,34 @@ def merge_tables():
         cursor.execute("DROP TABLE IF EXISTS merged")
         cursor.execute('''CREATE TABLE merged AS
                           SELECT ratings.user_id, ratings.movie_id, ratings.rating, movies.title, 
-                            movies.genres, movies.is_adult, movies.runtime_min, movies.release_date, 
-                            movies.video_release_date, movies.avgRating_IMDB, movies.numVotes_IMDB, 
+                            movies.genres, movies.is_adult, movies.runtime_min, movies.avgRating_IMDB, movies.numVotes_IMDB, 
                             movies.avgRating_users, movies.numVotes_users, ratings.timestamp, movies.IMDB_id
                           FROM ratings
                           JOIN movies ON ratings.movie_id = movies.movie_id''')
-
 # Print table name and number of entries in table, and the first 10 rows from the table 
-def print_table_info(table_name):
+def print_table_info(table_name, num_rows=10):
     with sqlite3.connect(database_name) as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT COUNT(*) FROM {}".format(table_name))
         print('The {} table has {} entries'.format(table_name, cursor.fetchone()[0]))
-        cursor.execute("SELECT * FROM {} LIMIT 10".format(table_name))
-        print(cursor.fetchall())                          
+        cursor.execute("SELECT * FROM {} LIMIT {}".format(table_name, num_rows))
+        print(cursor.fetchall())              
+        print('\n')            
+    return
 
-# Parrallel processing functions past this point (not done yet) 
+def create_small_file(big_file, small_file, num_lines):
+    with open(big_file, 'r', encoding='utf-8') as big, open(small_file, 'w', encoding='utf-8') as small:
+        next(big)  # Skip the first line
+        for _ in range(num_lines):
+            line = big.readline()
+            small.write(line)
 
-# This function is run in a separate process.
-# It reads and removes a row of data from the chunk if the movie does not exist in the 'movies' table
+"""
+------------------------------------------------------------------------------------------------------------------------
+ Parrallel processing functions to merge IMDB data with the movies table
+------------------------------------------------------------------------------------------------------------------------
+"""
+# Reads and removes a row of data from the chunk if the movie does not exist in the 'movies' table
 def process_data(chunk):
     results = []
     with sqlite3.connect(database_name) as conn:
@@ -154,76 +127,109 @@ def process_data(chunk):
                 results.append(row)            
     return results
 
-def split_data_into_chunks(file_name, chunk_size=1000):
-    # Read the entire file
+# Generator function to read data in chunks
+def read_data_in_chunks(file_name, chunk_size=1000):
+    chunk = []
     with open(file_name, 'r', encoding='utf-8') as f:
-        data = f.readlines()
+        next(f)  # Skip the first line
+        for i, line in enumerate(f):
+            if (i % chunk_size == 0 and i > 0):
+                yield chunk
+                chunk = []
+            chunk.append(line)
+        yield chunk  # yield the last chunk, which may be smaller than chunk_size
 
-    # Split the data into chunks
-    chunks = [data[i:i + chunk_size] for i in range(1, len(data), chunk_size)]
-
-    return chunks
-
-def update_moviesT_parrallel(file_name):
-    # Split the data into chunks
-    chunks = split_data_into_chunks(file_name)
-    time_1 = time.time()
-    print(f'The function took {time_1 - start_time} seconds to split the data into chunks.')
-
-    # Create a pool of worker processes
+# Adds IMDB data to the movies table in parallel processes
+def update_moviesT_parrallel(file_name, chunk_size=1000):
+    
+    start_time = time.time()
+    
+    # Create a pool of processes 
     with Pool() as p:
         # Use the pool to process the data in parallel
-        all_results = p.map(process_data, chunks)
-    time_2 = time.time()
-    print(f'The function took {time_2 - time_1} seconds to process the data in parallel.')
+        line_count = 0
+        for chunk in read_data_in_chunks(file_name, chunk_size):
+            line_count += len(chunk)
+            print(f'Processing lines {line_count - len(chunk) + 1} to {line_count}')
+            results = p.map(process_data, [chunk])
 
-    # Write the results to the database
+            # Write the results to the database
+            with sqlite3.connect(database_name) as conn:
+                cursor = conn.cursor()
+                for result in results[0]:
+                    if file_name == imdb_movie_file:
+                        # update movies table with result from process_data
+                        Imdb_id = int(result[0][2:])
+                        is_adult = None if result[4] == '\\N' else int(result[4])
+                        runtime_min = None if result[7] == '\\N' else int(result[7])
+                        if is_adult is not None or runtime_min is not None:
+                            cursor.execute("UPDATE movies SET is_adult=?, runtime_min=? WHERE IMDB_id=?", (is_adult, runtime_min, Imdb_id))
+                    
+                    elif file_name == imdb_rating_file:
+                        # update movies table with result from process_data
+                        Imdb_id = int(result[0][2:])
+                        avgRating = None if result[1] == '\\N' else float(result[1])
+                        nVotes = None if result[2] == '\\N' else int(result[2])
+                        if avgRating is not None or nVotes is not None:
+                            cursor.execute("UPDATE movies SET avgRating_IMDB=?, numVotes_IMDB=? WHERE IMDB_id=?", (avgRating, nVotes, Imdb_id))
+                conn.commit()
+                print(f'Updated {len(results[0])} rows')
+
+    print(f'Finished in {time.time() - start_time} seconds')
+
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+ Main function past this point to create and populate the database
+------------------------------------------------------------------------------------------------------------------------
+"""
+
+def main():
+
+    # write merged table to csv file
     with sqlite3.connect(database_name) as conn:
         cursor = conn.cursor()
-        for results in all_results:
-            for result in results:
-                if file_name == imdb_movie_file:
-                    # update movies table with result from process_data
-                    Imdb_id = int(result[0][2:])
-                    is_adult = None if result[4] == '\\N' else int(result[4])
-                    runtime_min = None if result[7] == '\\N' else int(result[7])
-                    if is_adult is not None or runtime_min is not None:
-                        cursor.execute("UPDATE movies SET is_adult=?, runtime_min=? WHERE IMDB_id=?", (is_adult, runtime_min, Imdb_id))
-                        print(f'Updated is_adult and runtime_min in movieFile to {is_adult} and {runtime_min} respectively with IMDB id {Imdb_id}')
-                    else:
-                        print('***!!!Could not update movie in movieFile with IMDB id ', Imdb_id)
-                elif file_name == imdb_rating_file:
-                    # update movies table with result from process_data
-                    Imdb_id = int(result[0][2:])
-                    avgRating = None if result[1] == '\\N' else float(result[1])
-                    nVotes = None if result[2] == '\\N' else int(result[2])
-                    if avgRating is not None or nVotes is not None:
-                        cursor.execute("UPDATE movies SET avgRating_IMDB=?, numVotes_IMDB=? WHERE IMDB_id=?", (avgRating, nVotes, Imdb_id))
-                        print(f'Updated numVotes and avgRating in ratingFile to {nVotes} and {avgRating} respectively with IMDB id {Imdb_id}')
-                    else:
-                        print('***!!!Could not update movie in ratingFile with IMDB id ', Imdb_id)
+        cursor.execute("SELECT * FROM merged")
+        with open('merged.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow([i[0] for i in cursor.description])
+            writer.writerows(cursor)
+            
+                    
+
+
+    # create_small_file(imdb_movie_file, 'small_imdb_movie_file.tsv', 100000)
+    # create_small_file(imdb_rating_file, 'small_imdb_rating_file.tsv', 100000)
+
+    # update_moviesT_parrallel('small_imdb_movie_file.tsv')
+    # update_moviesT_parrallel('small_imdb_rating_file.tsv')
     
-    time_3 = time.time()
-    print(f'The function took {time_3 - time_2} seconds to update the database.')
-    
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"The file took {elapsed_time} seconds to upload.")
+
+
+    #print('Creating and populating ratings table...')
+    #create_ratings_table()
+    #print_table_info('ratings')
+
+    #print('Creating and populating movies table...')
+    #create_movie_table()
+    #print_table_info('movies')
+
+    #print('Adding IMDB data to movies table...')
+    #update_moviesT_parrallel(imdb_rating_file)
+    #update_moviesT_parrallel(imdb_movie_file)
+    #print_table_info('movies')
+
+    # print('Merging ratings and movies tables...')
+    # merge_tables()
+    # print_table_info('merged')
+
+    print('Done!')
+
+
+   
 
 
 if __name__ == "__main__":
 
-    update_moviesT_parrallel(imdb_movie_file)
-    # print('Creating and populating movies table...')
-    # with sqlite3.connect(database_name) as conn:
-    #     cursor = conn.cursor()
-    #     cursor.execute("SELECT * FROM movies WHERE IMDB_id=7193448")
-    #     print(cursor.fetchall())    
-    #create_ratings_table()
-    #print_table_info('ratings')
-    #create_movie_table()
-    #print_table_info('movies')
-
-
-    # merge_tables()
-    # print_table_info('merged')
+    main()
+    
