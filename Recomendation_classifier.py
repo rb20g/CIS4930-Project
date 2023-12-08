09a737d3c5c86bf6db1001c325b567167ad11c9c
AdamSanch <adamsan1234@gmail.com>
2023-12-08 13:57:09 -0500

    add prints and produces train.csv and test.csv

--- Recomendation_classifier.py
+++ Recomendation_classifier.py
@@ -51,7 +51,7 @@ def preprocess_ratings(ratings):
     agg_ratings_GT100 = agg_ratings[agg_ratings['number_of_ratings'] > 100]
 
     # Visualize the aggregated ratings
-    visualize_ratings(agg_ratings_GT100)
+    #visualize_ratings(agg_ratings_GT100)
 
     # Merge ratings with movies that have more than 100 ratings, get rid of those with less than 100 ratings
     df_GT100 = pd.merge(ratings, agg_ratings_GT100[['title']], on='title', how='inner')
@@ -69,7 +69,7 @@ def create_user_item_matrix(df_GT100):
     matrix_norm = matrix.subtract(matrix.mean(axis=1), axis=0)
     return matrix_norm    
 
-# Function to calculate item similarity
+# Function to calculate item similarity matrix
 def calculate_item_similarity(matrix_norm):
     item_similarity = matrix_norm.T.corr()
     return item_similarity
@@ -82,7 +82,7 @@ def predict_rating(matrix_norm, item_similarity, picked_userid, picked_movie):
                                     .rename(columns={1: 'rating'})
     
     picked_movie_similarity_score = item_similarity[[picked_movie]].reset_index() \
-        .rename(columns={'American Pie (1999)': 'similarity_score'})
+        .rename(columns={picked_movie:'similarity_score'})
     
     picked_userid_watched_similarity = pd.merge(left=picked_userid_watched,
                                             right=picked_movie_similarity_score,
@@ -119,14 +119,56 @@ def item_based_rec(matrix_norm, item_similarity, picked_userid, number_of_simila
 def main():
     # Usage example:
     filepath = 'merged.csv'
-    ratings = load_data(filepath)
-    agg_ratings_GT100 = preprocess_ratings(ratings)
-    matrix_norm = create_user_item_matrix(agg_ratings_GT100)
-    item_similarity = calculate_item_similarity(matrix_norm)
 
-    # Predict rating for a specific user and movie
-    predicted_rating = predict_rating(matrix_norm, item_similarity, picked_userid=1, picked_movie='American Pie (1999)')
-    print(f'The predicted rating for American Pie (1999) by user 1 is {predicted_rating}\n')
+    # Load data
+    ratings = load_data(filepath)
+    print("Head of ratings DataFrame:")
+    print(ratings.head(), "\n")
+
+    # Preprocess ratings data by aggregating ratings by movie title, 
+    # calculate mean rating and number of ratings, and filter out movies with less than 100 ratings
+    agg_ratings_GT100 = preprocess_ratings(ratings)
+    print("Head of agg_ratings_GT100 DataFrame:")
+    print(agg_ratings_GT100.head(), "\n")
+
+    # Create a test set by taking one row for each user_id
+    test = agg_ratings_GT100.groupby('user_id').sample(n=1)
+    # print rows in test set with user_id = 1
+    print(test[test['user_id'] == 1], "\n")
+
+    # Create a training set by removing the test set rows from the matrix
+    train = agg_ratings_GT100.drop(test.index)
+
+    print("Head of train DataFrame:")
+    print(train.head(), "\n")
+    print("Head of test DataFrame:")
+    print(test.head(), "\n")
+
+    # Write the train and test sets to CSV files
+    train.to_csv('train.csv')
+    test.to_csv('test.csv')
+
+    
+    # Create user-item matrix and normalize the matrix by subtracting the mean rating of each movie from the ratings
+    matrix_norm = create_user_item_matrix(train)
+    print("Head of matrix_norm DataFrame:")
+    print(matrix_norm.head(), "\n")
+
+    item_similarity = calculate_item_similarity(matrix_norm)
+    print("Head of item_similarity DataFrame:")
+    print(item_similarity.head(), "\n")
+
+
+    # Predict rating for a specific user and movie in the test set
+    picked_userid = 1
+    # Get the movie title from the test set
+    picked_movie = test[test['user_id'] == picked_userid]['title'].values[0]
+    # Get actual rating from the test set
+    actual_rating = test[test['user_id'] == picked_userid]['rating'].values[0]
+
+    predicted_rating = predict_rating(matrix_norm, item_similarity, picked_userid, picked_movie)
+    print(f'The predicted rating for {picked_movie} by user {picked_userid} is {predicted_rating}\n')
+    print(f'The actual rating for {picked_movie} by user {picked_userid} is {actual_rating}\n')
 
     # Perform item-based recommendation for a specific user
     recommended_movies = item_based_rec(matrix_norm, item_similarity, picked_userid=1, number_of_similar_items=5, number_of_recommendations=3)
